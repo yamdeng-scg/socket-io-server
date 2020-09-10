b834f441a441b53649457256d198e501188cf972
yamdeng <yamdeng@gmail.com>
2020-09-10 09:44:21 +0900

    public add

---
+++ public/precache-manifest.6be6f52428c4cacb03a809b29ab8bf3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd5a0cfadfda3d9cccf2ce5d8dfe72e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7d38dc83436664ad401",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "2035de3015c9e9ca4818",
+    "url": "/static/css/main.bc8d77c3.chunk.css"
+  },
+  {
+    "revision": "e7d38dc83436664ad401",
+    "url": "/static/js/2.0844d4c9.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.0844d4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2035de3015c9e9ca4818",
+    "url": "/static/js/main.230b369c.chunk.js"
+  },
+  {
+    "revision": "9d8ff8c0a35d26d12ea9",
+    "url": "/static/js/runtime-main.6b34a334.js"
+  }
+]);
